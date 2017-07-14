618e16a38af5dd3302235226ca2a8f10edd880d2
Aurora <aurora@Blue-Designer-iMac.local>
2017-07-14 11:12:35 -0400

    day 1 changes made

--- js/vitality.js
+++ js/vitality.js
@@ -128,7 +128,7 @@ $(document).ready(function() {
 
     // Vide - Video Background Settings
     $('header.video').vide({
-        mp4: "mp4/camera.mp4",
+        mp4: "mp4/mtgVideo.mp4",
         poster: "img/agency/backgrounds/bg-mobile-fallback.jpg"
     }, {
         posterType: 'jpg'
